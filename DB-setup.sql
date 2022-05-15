b04a0c234194200f517dc0f81b44d061b146705b
harrisonhirsch <harrisonhirsch@gmail.com>
2022-05-15 11:39:51 -0700

    Setup Debugged and inserted drop table statements

--- DB-setup.sql
+++ DB-setup.sql
@@ -2,133 +2,158 @@
 --CSC366
 --GROUP MID ALPHABET
 
+DROP TABLE TextResponses;
+
+DROP TABLE OptionResponses;
+
+DROP TABLE AnswerOptions;
+
+DROP TABLE Questions;
+
+DROP TABLE Matches;
+
+DROP TABLE JobProfileValues;
+
+DROP TABLE DesiredValue;
+
+DROP TABLE RealValues;
+
+DROP TABLE ValueCharacteristics;
+
+DROP TABLE ONetJobs;
+
+DROP TABLE Profile;
+
+DROP TABLE Experiences;
+
+DROP TABLE Surveys;
+
+DROP TABLE Accounts;
+
 CREATE TABLE Accounts(
     id int,
     name varchar(45),
     email varchar(100),
     hashedPasssword varchar(32),
-    Primary Key(id),
+    PRIMARY KEY(id),
     UNIQUE (email)
-)
+);
 
 CREATE TABLE Surveys(
-  id INT AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(64),
   type VARCHAR(64),
   PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE Experiences(
-  id INT AUTO_INCREMENT,
-  accountId,
-  sId INT,
+  id INT NOT NULL AUTO_INCREMENT,
+  accountId INT,
+  sId INT NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY(accountId) REFERENCES Accounts(id),
   FOREIGN KEY(sId) REFERENCES Surveys(id)
-)
+);
 
 CREATE TABLE Profile(
-    profileId int AUTO_INCREMENT,
+    profileId int NOT NULL AUTO_INCREMENT,
     dateCreated date,
     metadata varchar(20),
     experienceID int,
-    profileType varchar(), --REAL OR DESIRED
+    profileType varchar(10),
     Primary Key (profileId),
     Foreign Key(experienceId) references Experiences(id)
-)
+);
 
 CREATE TABLE ONetJobs(
-    jobId int AUTO_INCREMENT,
+    jobId int NOT NULL AUTO_INCREMENT,
     jobTitle varchar(60),
     field varchar(40),
     description varchar(400),
     Primary Key(jobId),
     Unique(jobTitle, field)
-)
+);
 
 CREATE TABLE ValueCharacteristics(
-    id int AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     name varchar(40),
-    description varchar(512)
-)
+    description varchar(512),
+    Primary Key (id)
+);
 
 CREATE TABLE RealValues(
-  vcId int,
-  profileId int,
+  vcId int NOT NULL,
+  profileId int NOT NULL,
   realValue int,
   Primary Key(vcId, profileID),
   Foreign Key(vcId) references ValueCharacteristics(id),
   Foreign Key(profileID) references Profile(profileID)
-)
+);
 
 CREATE TABLE DesiredValue(
-  vcId int,
-  profileId int,
+  vcId int NOT NULL,
+  profileId int NOT NULL,
   importance int,
   desiredValue int,
   Primary Key(vcId, profileID),
   Foreign Key(vcId) references ValueCharacteristics(id),
   Foreign Key(profileID) references Profile(profileID)
-)
+);
 
 CREATE TABLE JobProfileValues(
-    jobId int,
-    vcId varchar(40),
+    jobId int NOT NULL,
+    vcId int NOT NULL,
     jobProfileValue int,
     Primary Key(jobId),
     Foreign Key(jobId) references ONetJobs(jobId),
     Foreign Key(vcId) references ValueCharacteristics(id)
-)
+);
 
 CREATE TABLE Matches(
-  jobProfileId int,
-  profileId int,
+  jobProfileId int NOT NULL,
+  profileId int NOT NULL,
   DateMatched date,
-  MatchSimiliarity decimal(10, 5)
+  MatchSimiliarity decimal(10, 5),
   Primary Key(jobProfileId, profileID),
-  Foreign Key(jobProfileId) references ONETJobs(id),
+  Foreign Key(jobProfileId) references ONetJobs(jobId),
   Foreign Key(profileID) references Profile(profileID)
-)
+);
 
 
 CREATE TABLE Questions(
-  sId INT,
-  position INT,
+  sId INT NOT NULL,
+  position INT NOT NULL,
   prompt VARCHAR(512),
   PRIMARY KEY(sId, position),
   FOREIGN KEY(sId) REFERENCES Surveys(id)
-)
+);
 
 CREATE TABLE AnswerOptions(
-  sId INT,
-  qPosition INT,
-  position INT,
+  sId INT NOT NULL,
+  qPosition INT NOT NULL, 
+  position INT NOT NULL,
   optionValue VARCHAR(512),
   PRIMARY KEY(sId, qPosition, position),
-  FOREIGN KEY(sId) REfERENCES Surveys(id),
-  FOREIGN KEY(qPosition) REFERENCES Questions(position)
-)
+  FOREIGN KEY(sId, qPosition) REFERENCES Questions(sId, position)
+);
 
 
 CREATE TABLE OptionResponses(
-  eId INT,
-  sId INT,
-  questionPosition INT,
-  optionPosition INT,
+  eId INT NOT NULL,
+  sId INT NOT NULL,
+  questionPosition INT NOT NULL,
+  optionPosition INT NOT NULL,
   PRIMARY KEY(eId, sId, questionPosition, optionPosition),
   FOREIGN KEY(eId) REFERENCES Experiences(id),
-  FOREIGN KEY(sId) REFERENCES Surveys(id),
-  FOREIGN KEY(questionPosition) REFERENCES Questions(position),
-  FOREIGN KEY(optionPosition) REFERENCES AnswerOptions(position)
-)
+  FOREIGN KEY(sId, questionPosition, optionPosition) REFERENCES AnswerOptions(sId, qPosition, position)
+);
 
 CREATE TABLE TextResponses(
-  eId INT,
-  sId INT,
-  questionPosition INT,
+  eId INT NOT NULL,
+  sId INT NOT NULL,
+  questionPosition INT NOT NULL,
   answer VARCHAR(256),
   PRIMARY KEY(eId, sId, questionPosition),
   FOREIGN KEY(eId) REFERENCES Experiences(id),
-  FOREIGN KEY(sId) REFERENCES Surveys(id),
-  FOREIGN KEY(questionPosition) REFERENCES Questions(position)
-)
+  FOREIGN KEY(sId, questionPosition) REFERENCES Questions(sId, position)
+);
