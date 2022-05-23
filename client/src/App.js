c463f25e1cd51df50b03bc8f428e9e0dd7d36707
KennaReed <mreed16@calpoly.edu>
2022-05-23 12:33:15 -0700

    navBar functional but not cute

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/NavBar/navBar";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Navbar />} />
+            <Route exact path="/surveys" element={<Navbar />} />
+            <Route path="/profiles" element={<Navbar />} />
+            <Route path="/users" element={<Navbar />} />
+            <Route path="/login" element={<Navbar />} />
+            <Route path="/results" element={<Navbar />} />
+            <Route path="/account" element={<Navbar />} />
+            <Route path="/createSurvey" element={<Navbar />} />
+            <Route path="*" element={<p>404</p>} />
+          </Routes>
+        </Router>
+      </ChakraProvider>
     </div>
   );
 }
---
+++ client/src/components/NavBar/navBar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link as ReactLink } from "react-router-dom";
+import { Box, Flex, Spacer, HStack, Heading, Link } from "@chakra-ui/react";
+
+export default function Navbar(props) {
+  return (
+    <Flex pt={5} pb={5} bg="#5480D4">
+      <HStack pr={10}>
+        <Box mr={2}>
+          <Link as={ReactLink} _activeLink={{ fontWeight: "bold" }} to="/">
+            {" "}
+            <Heading size="lg" color="white">
+              {" "}
+              Dashboard{" "}
+            </Heading>{" "}
+          </Link>
+        </Box>
+        <Box>
+          <Link as={ReactLink} to="/surveys">
+            {" "}
+            <Heading size="lg" color="white">
+              {" "}
+              Surveys{" "}
+            </Heading>{" "}
+          </Link>
+        </Box>
+        <Box mr={2}>
+          <Link
+            as={ReactLink}
+            _activeLink={{ fontWeight: "bold" }}
+            to="/profiles"
+          >
+            {" "}
+            <Heading size="lg" color="white">
+              {" "}
+              Profiles{" "}
+            </Heading>{" "}
+          </Link>
+        </Box>
+        <Box mr={2}>
+          <Link as={ReactLink} _activeLink={{ fontWeight: "bold" }} to="/users">
+            {" "}
+            <Heading size="lg" color="white">
+              {" "}
+              Users{" "}
+            </Heading>{" "}
+          </Link>
+        </Box>
+        <Box mr={2}>
+          <Link
+            as={ReactLink}
+            _activeLink={{ fontWeight: "bold" }}
+            to="/results"
+          >
+            {" "}
+            <Heading size="lg" color="white">
+              {" "}
+              Results{" "}
+            </Heading>{" "}
+          </Link>
+        </Box>
+      </HStack>
+      <Box mr={2}>
+        <Link as={ReactLink} _activeLink={{ fontWeight: "bold" }} to="/account">
+          {" "}
+          <Heading size="lg" color="white">
+            {" "}
+            Account{" "}
+          </Heading>{" "}
+        </Link>
+      </Box>
+    </Flex>
+  );
+}
